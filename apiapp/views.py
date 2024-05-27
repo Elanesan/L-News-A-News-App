0476c33121fdf0af6be6d97bdb62a9d28cf03079
Elanesan <elanesankumaravel@gmail.com>
2024-05-27 18:26:58 +0530

    mdify: all

--- apiapp/views.py
+++ apiapp/views.py
@@ -6,5 +6,6 @@ import requests
 
 
 def index(request):
-    response = requests.get("https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata").json() 
-    return render(request,'index.html',{'response':response})   
+    response = requests.get("https://newsapi.org/v2/everything?q=tesla&from=2024-04-27&sortBy=publishedAt&apiKey=209b70796c9e4fc082c39d05f423276e").json()
+    articles = response.get('articles', [])
+    return render(request, 'index.html', {'articles': articles})
